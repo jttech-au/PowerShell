7cf7ea41bcadf601eb3f68f32633ceb8881e78f4
JT Tech <162686276+jttech-au@users.noreply.github.com>
2026-01-12 21:37:34 +1100

    Update Export-Import-DHCPServer.ps1

--- Export-Import-DHCPServer.ps1
+++ Export-Import-DHCPServer.ps1
@@ -1,3 +1,2 @@
-﻿Export-DhcpServer -ComputerName 1436DC01.curric.mount-pleasant-ps.wan -Leases -File "C:\Users\$env:USERNAME\Desktop\dhcpexp.xml" -verbose
-
-Import-DhcpServer -ComputerName 0427DC01.curric.mount-clear-ps.wan -Leases -File "C:\Users\$env:USERNAME\Desktop\dhcpexp.xml" -BackupPath "C:\Users\$env:USERNAME\Desktop\backup\" -Verbose
+﻿Export-DhcpServer -ComputerName DC01.curric.school-ps.wan -Leases -File "C:\Users\$env:USERNAME\Desktop\dhcpexp.xml" -verbose
+Import-DhcpServer -ComputerName DC01.curric.school-ps.wan -Leases -File "C:\Users\$env:USERNAME\Desktop\dhcpexp.xml" -BackupPath "C:\Users\$env:USERNAME\Desktop\backup\" -Verbose
